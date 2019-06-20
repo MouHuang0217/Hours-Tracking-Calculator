94707a244c4b00ca20613a68285a143ca7d1c670
lolatme4667 <moulue4667@yahoo.com>
2019-06-20 16:30:03 -0700

    added multiple Inputs

--- app/src/main/java/com/example/calendarv2/TimePickerFragment.java
+++ app/src/main/java/com/example/calendarv2/TimePickerFragment.java
@@ -6,14 +6,27 @@ import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.text.format.DateFormat;
 import android.util.Log;
+import android.widget.EditText;
 import android.widget.TimePicker;
 
 import androidx.fragment.app.DialogFragment;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 public class TimePickerFragment extends DialogFragment implements TimePickerDialog.OnTimeSetListener {
-    public TimePickerFragment() {
+    private EditText time;
+    private EditText other_time;
+    private EditText hoursTV;
+    private EditText output;
+    private EditText payPerHour;
+    public TimePickerFragment(EditText timeTV,EditText otherTV,EditText hoursTV, EditText outputTV) {
+        this.time = timeTV;
+        this.other_time = otherTV;
+        this.hoursTV = hoursTV;
+        this.output = outputTV;
         Log.i("Timepicker","createdFragment");
     }
     @Override
@@ -31,7 +44,43 @@ public class TimePickerFragment extends DialogFragment implements TimePickerDial
 
     @Override
     public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-        // Do something with the time chosen by the user
+
+        String AM_PM ;
+        if(hourOfDay < 12) {
+            AM_PM = "AM";
+        } else {
+            AM_PM = "PM";
+        }
+        if (minute < 10){
+            time.setText(hourOfDay + ":" + minute +"0 " + AM_PM);
+        }
+        else{
+        time.setText(hourOfDay + ":" + minute + " " + AM_PM);
+        }
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm a");
+
+        Log.i("starttimes:" , String.valueOf(time.getText()));
+        Log.i("endTime: ", String.valueOf(other_time.getText()));
+        Date timeOne = null;
+        Date timeTwo = null;
+        try {
+            // calculate the difference in time from Start time to end time
+            timeOne = simpleDateFormat.parse(String.valueOf(time.getText()));
+            timeTwo = simpleDateFormat.parse(String.valueOf(other_time.getText()));
+            long difference = timeTwo.getTime() - timeOne.getTime();
+
+            double days = (double) (difference / (1000*60*60*24));
+            double hours = (double) ((difference - (1000*60*60*24*days)) / (1000*60*60));
+            double min = (double) (difference - (1000*60*60*24*days) - (1000*60*60*hours)) / (1000*60);
+            hours = (hours < 0 ? -hours : hours);
+
+            //set the total hours worked into the Textview of hours
+            hoursTV.setText(String.valueOf(hours));
+           // output.setText(String.valueOf(totalPay));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
     }
 
 }
--- app/src/main/java/com/example/calendarv2/WorkHoursActivity.java
+++ app/src/main/java/com/example/calendarv2/WorkHoursActivity.java
@@ -8,19 +8,26 @@ import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.DialogFragment;
 
-public class WorkHoursActivity extends AppCompatActivity {
+public class WorkHoursActivity extends AppCompatActivity implements EditText.OnClickListener  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-
+        //initialize bundle to pass data back into main frame
         Bundle startEndTime = getIntent().getExtras();
-
         Intent passData_Intent = new Intent(this, WorkHoursActivity.class);
 
-        EditText startTimeTV = (EditText) findViewById(R.id.startTimeInput);
+        // get the startime textview and apply onclick and work on it to change data
+        final EditText startTimeTV = (EditText) findViewById(R.id.startTimeInput);
+        startTimeTV.setOnClickListener(this);
+        final EditText endTimeTV = (EditText) findViewById(R.id.endTimeInput);
+        endTimeTV.setOnClickListener(this);
 
+        final EditText payInput = (EditText)findViewById(R.id.payPerHourInput);
+    }
+//TODO add Calculate button, and finish button - > calculate
+/*
         String startTime = "startTime:";
         passData_Intent.putExtra("startTime",startTime);
 
@@ -28,11 +35,32 @@ public class WorkHoursActivity extends AppCompatActivity {
         passData_Intent.putExtra("endTime",endTime);
 
         setResult(RESULT_OK,passData_Intent);
+*/
 
-    }
 
-    public void openTimePicker(View view) {
-        DialogFragment timePicker = new TimePickerFragment();
+
+    @Override
+    public void onClick(View view) {
+        EditText InputTV = null;
+        EditText OtherTV = null;
+        switch(view.getId()){
+            case R.id.startTimeInput:
+                InputTV = (EditText) findViewById(R.id.startTimeInput);
+                OtherTV = (EditText) findViewById(R.id.endTimeInput);
+                break;
+            case R.id.endTimeInput:
+                InputTV = (EditText) findViewById(R.id.endTimeInput);
+                OtherTV = (EditText) findViewById(R.id.startTimeInput);
+                break;
+            default:
+                throw new RuntimeException("Unknown EditText ID");
+        }
+
+        EditText TotalOutput = (EditText) findViewById(R.id.totalEarnedOutput);
+        EditText payPerHourInput = (EditText) findViewById(R.id.payPerHourInput);
+        final EditText totalTimeTV = (EditText) findViewById(R.id.totalHoursCalculated);
+        DialogFragment timePicker = new TimePickerFragment(InputTV,OtherTV,totalTimeTV,TotalOutput);
         timePicker.show(getSupportFragmentManager(),"time picker");
     }
+
 }
